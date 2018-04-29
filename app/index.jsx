2af37a27fcf2ef938e3c30fde5e6dc4a4570305d
Nazim Jamil-Mir <nazimjamil@gmail.com>
2018-04-29 19:13:05 +0100

    Update ESLint settings to play nice with flow 🌞

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,16 @@
 // @flow
 
-import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from 'router';
 import 'styles/main.css';
 
-let node = document.getElementById('root');
+const node = document.getElementById('root');
 
 if (node) {
   ReactDOM.render(
     Routes,
-    node
+    node,
   );
 } else {
-  console.warn('Node not found ☠️');
+  console.warn('Node not found ☠️'); // eslint-disable-line
 }
--- app/router.jsx
+++ app/router.jsx
@@ -3,11 +3,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+type RouteT = {
+  match: {
+    params: {
+      id: string,
+    },
+  },
+};
+
 const Home = () => (
   <h1>Hello, new world!</h1>
 );
 
-const Param = ({ match }) => (
+const Param = ({ match }: RouteT) => (
   <h1>Hello, {match.params.id}!</h1>
 );
 
@@ -15,8 +23,8 @@ const Routes = (
   <Router>
     <div>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/user">Parameter</Link>
+        <Link to="/" href="/" >Home</Link>
+        <Link to="/user" href="/user" >Parameter</Link>
       </nav>
       <Route exact path="/" component={Home} />
       <Route path="/:id" component={Param} />
