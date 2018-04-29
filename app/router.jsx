9dd5ab3a8b3efa9acdbc01d49bcbb168141876ca
Nazim Jamil-Mir <nazimjamil@gmail.com>
2018-04-29 09:43:19 +0100

    Update eslint config to parse JSX 👍 Add demo nav 👏🏼

--- app/router.jsx
+++ app/router.jsx
@@ -1,19 +1,23 @@
 // @flow
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const Home = () => (
   <h1>Hello, new world!</h1>
-)
+);
 
 const Param = ({ match }) => (
   <h1>Hello, {match.params.id}!</h1>
-)
+);
 
 const Routes = (
   <Router>
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/user">Parameter</Link>
+      </nav>
       <Route exact path="/" component={Home} />
       <Route path="/:id" component={Param} />
     </div>
