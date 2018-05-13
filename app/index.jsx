58e022f7c5d77e876bee5380b48ba413664174c5
Nazim Jamil-Mir <nazimjamil@gmail.com>
2018-05-13 18:23:28 +0100

    Add react-snap to generate static site.

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,17 @@
 // @flow
 
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 import Routes from 'router';
 import 'styles/main.css';
 
 const node = document.getElementById('root');
 
 if (node) {
-  ReactDOM.render(
-    Routes,
-    node,
-  );
+  if (node.hasChildNodes()) {
+    hydrate(Routes, node);
+  } else {
+    render(Routes, node);
+  }
 } else {
   console.warn('Node not found ☠️'); // eslint-disable-line
 }
