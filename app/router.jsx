41369c663ebc472bca9e171cf04ba4efe2de29f1
Nazim Jamil-Mir <nazimjamil@gmail.com>
2018-05-20 14:51:35 +0100

    Add context consumer example

--- app/router.jsx
+++ app/router.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import AppContext from 'appContext';
 
 type RouteT = {
   match: {
@@ -19,15 +20,23 @@ const Param = ({ match }: RouteT) => (
   <h1>Hello, {match.params.id}!</h1>
 );
 
+const Data = () => (
+  <AppContext.Consumer>
+    {context => <p>{ JSON.stringify(context[0])}</p>}
+  </AppContext.Consumer>
+);
+
 const Routes = (
   <Router>
     <div>
       <nav>
         <Link to="/" href="/" >Home</Link>
         <Link to="/user" href="/user" >Parameter</Link>
+        <Link to="/data" href="/data" >Context</Link>
       </nav>
       <Route exact path="/" component={Home} />
       <Route path="/:id" component={Param} />
+      <Route path="/data" component={Data} />
     </div>
   </Router>
 );
